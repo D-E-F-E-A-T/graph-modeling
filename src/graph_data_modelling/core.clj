31032c94b3b3b2e36e70676e175679ada309ef2d
Geem7n <gadebesin@gmail.com>
2016-08-08 23:07:44 +0100

    added proj

---
+++ src/graph_data_modelling/core.clj
@@ -0,0 +1,13 @@
+(ns graph-data-modelling.core
+  (:require [ubergraph.core :as uber]))
+
+(def my-graph (uber/digraph [:a :b]))
+
+
+(def airport
+  (uber/graph
+    [:city {:name "Chicago" :country "United States of America"}]
+    [:flight {:code "AC702" :carrier "Air Canada" :duration 96 :source-airport-code "ORD" :destination-aiport "JFK" :departure 1130 :arrival 1226}]
+    [:city :flight]))
+
+(uber/pprint airport) 
